cebe7e788209d59a48449e9c46e27e615022ed63
Emperor <lq.chinh88@gmail.com>
2025-08-06 14:00:55 +0700

    show google book images

--- lib/widgets/book_card.dart
+++ lib/widgets/book_card.dart
@@ -189,6 +189,20 @@ class GoogleBookCard extends StatelessWidget {
 
   const GoogleBookCard({super.key, required this.book});
 
+  String _convertToHttps(String url) {
+    // Use a proxy service to bypass CORS restrictions for Google Books images
+    if (url.contains('books.google.com')) {
+      // Use images.weserv.nl as a proxy to bypass CORS
+      String encodedUrl = Uri.encodeComponent(url);
+      return 'https://images.weserv.nl/?url=$encodedUrl&w=160&h=240&fit=cover';
+    }
+    // For non-Google Books URLs, just convert HTTP to HTTPS
+    if (url.startsWith('http://')) {
+      return url.replaceFirst('http://', 'https://');
+    }
+    return url;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -212,7 +226,7 @@ class GoogleBookCard extends StatelessWidget {
                   ? ClipRRect(
                       borderRadius: BorderRadius.circular(8),
                       child: Image.network(
-                        book.imageUrl!,
+                        _convertToHttps(book.imageUrl!),
                         fit: BoxFit.cover,
                         errorBuilder: (context, error, stackTrace) {
                           return Container(
